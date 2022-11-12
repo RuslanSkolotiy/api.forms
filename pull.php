f5ac0a4eb62c4a4511ee9fc66ccec999f7175987
Ruslan Skoloty <RSkoloty@renins.com>
2022-11-12 18:12:46 +0300

    fix

--- pull.php
+++ pull.php
@@ -1,4 +1,4 @@
 <?php
-system("git pull origin main2>&1");
-system("git status2>&1");
-system("composer update2>&1");
+system("git pull origin main 2>&1");
+system("git status 2>&1");
+system("composer update 2>&1");
