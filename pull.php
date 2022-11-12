86c178e2bad5f5a416068dcd8a2490789d5e0e47
Ruslan Skoloty <RSkoloty@renins.com>
2022-11-12 18:23:40 +0300

    composer

--- pull.php
+++ pull.php
@@ -1,6 +1,7 @@
 <?php
+$composerHome = __DIR__ . "/../buzzz_forms_vendor/";
 echo "<pre>";
 system("git pull origin main 2>&1");
 system("git status 2>&1");
-system("composer update 2>&1");
+system("export COMPOSER_HOME=$composerHome && composer update 2>&1");
 echo "</pre>";
