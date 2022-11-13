8169e93f2668571fec553d240e09a6fa6b8766bd
Ruslan Skoloty <RSkoloty@renins.com>
2022-11-14 01:19:33 +0300

    cors

--- src/App/Cors.php
+++ src/App/Cors.php
@@ -14,5 +14,6 @@ class Cors {
         header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
         header('Access-Control-Max-Age: 1000');
         header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+        header('Access-Control-Allow-Credentials: true');
     }
 }
