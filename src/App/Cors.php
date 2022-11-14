2855ef4a8cb78f1db8ff5723383be0c5c8348d84
Ruslan Skoloty <RSkoloty@renins.com>
2022-11-14 13:08:34 +0300

    Cors

--- src/App/Cors.php
+++ src/App/Cors.php
@@ -2,7 +2,8 @@
 
 namespace Buzzz\App;
 
-class Cors {
+class Cors
+{
     /**
      * Example $allowedOrigins = array('(http(s)://)?(www\.)?my\-domain\.com');
      * @param $allowedOrigins
@@ -10,10 +11,18 @@ class Cors {
      */
     public static function init($allowedOrigin)
     {
-        header('Access-Control-Allow-Origin: ' . $allowedOrigin);
-        header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-        header('Access-Control-Max-Age: 1000');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-        header('Access-Control-Allow-Credentials: true');
+        if (isset($allowedOrigin)) {
+            header("Access-Control-Allow-Origin: {$allowedOrigin}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');    // cache for 1 day
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                header("Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS");
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            exit(0);
+        }
     }
 }
